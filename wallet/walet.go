d99c63709f9e52209c10a3a1769ba6b5408acf89
Your Name <victoradetunji16@gmail.com>
2025-11-27 14:07:29 +0100

    ship: introduce private and public key concept

--- wallet/walet.go
+++ wallet/walet.go
@@ -1,4 +1,24 @@
 package wallet
 
-type wallet struct {
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+)
+
+type Wallet struct {
+	privateKey *ecdsa.PrivateKey
+	publicKey  *ecdsa.PublicKey
+}
+
+func NewWallet() *Wallet {
+	w := new(Wallet)
+	privateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	w.privateKey = privateKey
+	w.publicKey = &w.privateKey.PublicKey
+	return w
+}
+
+func (w *Wallet) PrivateKey() *ecdsa.PrivateKey {
+	return w.privateKey
 }
