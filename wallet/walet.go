13d6502b56eb6a3bb48561e3665767e867b55b09
Your Name <victoradetunji16@gmail.com>
2025-11-28 22:03:51 +0100

    add: 2. compute EC public key

--- wallet/walet.go
+++ wallet/walet.go
@@ -4,9 +4,10 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
-	"fmt"
+	"crypto/sha256"
 	"github.com/btcsuite/btcutil/base58"
 	"golang.org/x/crypto/ripemd160"
+	// "fmt"
 )
 
 type Wallet struct {
@@ -23,6 +24,11 @@ func NewWallet() *Wallet {
 	w.publicKey = &w.privateKey.PublicKey
 
 	// 2. compute the EC public key by doing a secp256k1 point multiply (g * priv).
+	h2 := sha256.New()
+	h2.Write(W.publicKey.X.Bytes())
+	h2.Write(W.publicKey.Y.Bytes())
+	digest2 := h2.Sum(nil)
+
 	// 3. optionally compress the public key to 33 bytes (02/03 + x).
 	// 4. SHA-256 the public key.
 	// 5. RIPEMD-160 the SHA-256 output → pubkey hash (20 bytes).
@@ -34,16 +40,16 @@ func NewWallet() *Wallet {
 	return w
 }
 
-func (w *Wallet) PrivateKey() *ecdsa.PrivateKey {
-	return w.privateKey
-}
-func (w *Wallet) PrivateKeyStr() string {
-	return fmt.Sprintf("%x", w.privateKey.D.Bytes())
-}
+// func (w *Wallet) PrivateKey() *ecdsa.PrivateKey {
+// 	return w.privateKey
+// }
+// func (w *Wallet) PrivateKeyStr() string {
+// 	return fmt.Sprintf("%x", w.privateKey.D.Bytes())
+// }
 
-func (w *Wallet) PublicKey() *ecdsa.PublicKey {
-	return w.publicKey
-}
-func (w *Wallet) PublicKeyStr() string {
-	return fmt.Sprintf("%x%x", w.publicKey.X.Bytes(), w.publicKey.Y.Bytes())
-}
+// func (w *Wallet) PublicKey() *ecdsa.PublicKey {
+// 	return w.publicKey
+// }
+// func (w *Wallet) PublicKeyStr() string {
+// 	return fmt.Sprintf("%x%x", w.publicKey.X.Bytes(), w.publicKey.Y.Bytes())
+// }
