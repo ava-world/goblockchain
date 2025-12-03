c60afedefacb2c8d0f26f78382a59e4d9d4e8be7
Your Name <victoradetunji16@gmail.com>
2025-12-03 14:23:40 +0100

    ship: distinguish wallet and blockchain address

--- main.go
+++ main.go
@@ -10,7 +10,16 @@ func init() {
 }
 
 func main() {
-	w := wallet.NewWallet()
-	t := wallet.NewTransaction(w.PrivateKey(), w.PublicKey(), w.BlockchainAddress(), "B", 1.0)
+	walletM := wallet.NewWallet()
+	walletA := wallet.NewWallet()
+	walletB := wallet.NewWallet()
 
+	//Wallet
+	t := wallet.NewTransaction(walletA.PrivateKey(), walletA.PublicKey(), walletA.BlockchainAddress(), walletB.BlockchainAddress() "B", 1.0)
+
+	//Blockchain
+	blockchain := block.NewBlockchain(walletM.BlockchainAddress())
+	isAdded := blockchain.AddTransaction(walletA.BlockchainAdress(), walletB.BlockchainAddress(), 1.0, 
+		walletA.PublicKey(), t.GenerateSignature())
+	fmt.Println("Added? ", isAdded)	
 }
