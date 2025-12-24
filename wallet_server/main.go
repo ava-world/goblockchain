e03324cbf7bc4763d21ae639e8fa8e3e57170c53
Your Name <victoradetunji16@gmail.com>
2025-12-24 08:32:41 +0100

    fix: updated package main code

--- wallet_server/main.go
+++ wallet_server/main.go
@@ -9,9 +9,10 @@ func init() {
 	log.SetPrefix("WalletServer: ")
 }
 func main() {
-	port := flag.String("gateway", "http://127.0.0.1:5000", "Blockchain Gateway")
+	port := flag.Uint("port", 8080, "TCP Port Number for Wallet Server")
+	gateway := flag.String("gateway", "http://127.0.0.1:5000", "Blockchain Gateway")
 	flag.Parse()
 
-	app := NewWalletServer(uint16(port), *gateway)
+	app := NewWalletServer(uint16(*port), *gateway)
 	app.Run()
 }
