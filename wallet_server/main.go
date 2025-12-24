90310687388a13be441f419b449220a221d0ffc4
Your Name <victoradetunji16@gmail.com>
2025-12-24 08:29:27 +0100

    add: wrote package main logic

---
+++ wallet_server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+func init() {
+	log.SetPrefix("WalletServer: ")
+}
+func main() {
+	port := flag.String("gateway", "http://127.0.0.1:5000", "Blockchain Gateway")
+	flag.Parse()
+
+	app := NewWalletServer(uint16(port), *gateway)
+	app.Run()
+}
